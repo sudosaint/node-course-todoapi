1237c068899dae99a99215ddcf678e423db08c66
sudosaint <f20140045@goa.bits-pilani.ac.in>
2018-06-27 22:47:31 +0530

    Add post todos route and refactor code

---
+++ server/models/users.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var Users = mongoose.model('Users',{
+  email:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  }
+});
+
+module.exports = {Users};
